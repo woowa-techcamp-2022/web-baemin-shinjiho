185fa8b8df9eef03ba2e652c4401b5a282adc6d5
Jiho Shin <ziho@kakao.com>
2022-07-06 16:27:46 +0900

    feat: :sparkles: 헤더 텍스트 동적으로 변경되도록 설정

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', (req, res) => {
-  res.render('pages/main/main', { title: '메인페이지' });
+  res.render('pages/main/main', { title: 'My배민' });
 });
 
 module.exports = router;
