3e4dbf9e9bc53efe9cc0e0480c18a03786385184
Jiho Shin <ziho@kakao.com>
2022-07-06 22:25:07 +0900

    feat: :sparkles: 로그인 상태면 초기화면에 닉네임 띄우기

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,15 @@
 const express = require('express');
+const db = require('../lib/db');
 const router = express.Router();
 
 router.get('/', (req, res) => {
-  res.render('pages/main/main', { title: 'My배민' });
+  const userSession = req.session;
+
+  let userData = null;
+  if (userSession.email) {
+    userData = db.get('users').find({ email: userSession.email }).value();
+  }
+  res.render('pages/main/main', { title: 'My배민', userData });
 });
 
 module.exports = router;
