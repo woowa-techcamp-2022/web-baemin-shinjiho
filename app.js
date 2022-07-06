842105e48fb27e7a2291071434ccb27fe845a2b0
Jiho Shin <ziho@kakao.com>
2022-07-06 15:10:53 +0900

    feat: :sparkles: 404 페이지 구현

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const path = require('path');
+const cookieParser = require('cookie-parser');
 const low = require('lowdb');
 const FileSync = require('lowdb/adapters/FileSync');
-const mainRoute = require('./routes');
+const mainRouter = require('./routes');
 
 // DB
 const adapter = new FileSync('db.json');
@@ -11,14 +12,18 @@ const db = low(adapter);
 const app = express();
 const port = process.env.PORT || 8080;
 app.set('view engine', 'pug');
-app.set('views', path.join(__dirname, 'views/pages/'));
+app.set('views', path.join(__dirname, '/views'));
 
 app.use(express.static(path.join(__dirname, '/public')));
-
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
 
-app.use('/', mainRoute);
+app.use('/', mainRouter);
+
+app.use((req, res, next) => {
+  res.status(404).render('404');
+});
 
 app.listen(port, () => {
   console.log(`Server Connected:  http://localhost:${port}`);
--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', (req, res) => {
-  res.render('main/main');
+  res.render('pages/main/main', { title: '메인페이지' });
 });
 
 module.exports = router;
