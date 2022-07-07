d956f823b147ef9e4d76b45c57a6a82756f6bf8f
Jiho Shin <ziho@kakao.com>
2022-07-07 10:23:27 +0900

    feat: :sparkles: 필수 체크박스 모두 선택되면 다음으로 버튼 활성화

--- public/js/signup-service-term.js
+++ public/js/signup-service-term.js
@@ -1,6 +1,7 @@
 const init = () => {
   const $checkAllAgree = document.getElementById('check-all-agree-input');
   const $checkboxList = document.getElementById('service-term-checkbox-list');
+  const $nextButton = document.getElementById('service-term-next-button');
 
   const $toggleCheckbox = ($checkbox) => {
     $checkbox.checked = !$checkbox.checked;
@@ -14,7 +15,27 @@ const init = () => {
     });
   };
 
+  const toggleNextButton = (isAllRequired) => {
+    if (isAllRequired) {
+      $nextButton.style.backgroundColor = '#2ac1bc';
+      return;
+    }
+    $nextButton.style.backgroundColor = '#bbbbbb';
+  };
+
+  const checkRequiredCheckbox = () => {
+    const requiredCheckboxList = $checkboxList.querySelectorAll('.check-required');
+    const checkedRequiredCheckboxList = $checkboxList.querySelectorAll('.check-required:checked');
+
+    if (requiredCheckboxList.length === checkedRequiredCheckboxList.length) {
+      toggleNextButton(true);
+      return;
+    }
+    toggleNextButton(false);
+  };
+
   $checkAllAgree.addEventListener('change', toggleAllCheckbox);
+  $checkboxList.addEventListener('change', checkRequiredCheckbox);
 };
 
 init();
